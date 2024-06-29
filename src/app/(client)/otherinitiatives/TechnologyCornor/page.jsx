d1d02084f47c25adb7b3b9386bdef9aab7b07a18
Shadil Rayyan <shadilrayyan2@gmail.com>
2024-06-29 15:15:59 +0530

    second commit added initaiveis

---
+++ src/app/(client)/otherinitiatives/TechnologyCornor/page.jsx
@@ -0,0 +1,37 @@
+const TechnologyCornerOtherInviat = () => {
+    return (
+      <div className="w-full relative bg-white h-2055 overflow-hidden text-center text-48 text-black font-roboto">
+        <div className="absolute top-186 left-273 leading-120 font-extrabold inline-block w-894">
+          Technology Corner
+        </div>
+        <div className="absolute top-302 left-203 w-1050 h-647" />
+        <div className="absolute top-1083 left-41 text-24 font-arial text-left inline-block w-1367">
+          <p className="mt-0 mb-1">
+            Prof K Viswanathan, who was part of our Mechanical engineering
+            department, was a leading light and a guide for many of the students
+            during his tenure. He remained an influencer and a guide even after he
+            retired from the college. Through his involvement at the IRTC, he had
+            stamped a strong mark in alternative and sustainable approaches to
+            engineering practices.
+          </p>
+          <p className="mt-0 mb-1">
+            As a memorial to the beloved teacher, Darsana set up a technology
+            corner at Palakkad district public library called Prof. K. Viswanathan
+            Memorial Technology Corner. The technology corner has reference books
+            in engineering, management, and related subjects. The technology
+            corner was inaugurated by Mr. K.D Prasenan, (Hon. MLA Alathur
+            constituency). The facility is being utilized by students across the
+            district.
+          </p>
+        </div>
+        <img
+          className="absolute top-320 left-85 w-1269 h-611 object-cover"
+          alt=""
+          src="Technology Corner other inviaties hero image.png"
+        />
+      </div>
+    );
+  };
+  
+  export default TechnologyCornerOtherInviat;
+  
